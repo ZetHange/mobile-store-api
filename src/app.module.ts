0068ed52180c80dd1cb6a0a79b33c6925a396bdc
ZetHange <maksselles@gmail.com>
2023-01-08 12:44:38 +0300

    Remove db

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,15 +8,15 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       envFilePath: `.env`,
     }),
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRESS_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRESS_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      autoLoadModels: true,
-    }),
+    // SequelizeModule.forRoot({
+    //   dialect: 'postgres',
+    //   host: process.env.POSTGRES_HOST,
+    //   port: Number(process.env.POSTGRESS_PORT),
+    //   username: process.env.POSTGRES_USER,
+    //   password: process.env.POSTGRESS_PASSWORD,
+    //   database: process.env.POSTGRES_DB,
+    //   autoLoadModels: true,
+    // }),
   ],
 })
 export class AppModule {}
